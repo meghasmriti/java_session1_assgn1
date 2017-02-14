e5fcb78814cb54fcc82cbc15043e174590040a5e
meghasmriti <megha.smriti@yahoo.co.in>
2017-02-15 00:24:25 +0530

    Assgnment1.4

---
+++ ascii.java
@@ -0,0 +1,14 @@
+package megha_java;
+
+public class ascii {
+	public static void main(String s[])
+	{
+	  for(int i = 65; i<=90; i++)
+	  {
+		 char ascii = (char)i;
+	     System.out.println(i + "-" + ascii);
+	  }
+	  
+	}
+
+}
