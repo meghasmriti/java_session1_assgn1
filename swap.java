e4458bb877cc5b96a487c94811f004f531b63c3a
meghasmriti <megha.smriti@yahoo.co.in>
2017-02-15 00:13:39 +0530

    session1_assgnmnt3

---
+++ swap.java
@@ -0,0 +1,14 @@
+package megha_java;
+
+public class swap {
+
+	public static void main(String s[])
+	{
+	  int a = 10, b =20;
+	  a = a+b ;
+	  b = a-b;
+	  a = a - b;
+	  System.out.println("Swapped value of  a is " +a);
+	  System.out.println("Swapped value of  b is " +b);
+	}
+}
